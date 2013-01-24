15da57e1915c1f0f46be4cbd7f1c38e3105a252e
Erick Navarro <erickxls@gmail.com>
2013-01-24 09:04:22 -0500

    added from content after get request

--- apps/admin/views/measurement.py
+++ apps/admin/views/measurement.py
@@ -17,7 +17,11 @@ class MeasurementListView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super(MeasurementListView, self).get_context_data(**kwargs)
-        context['search_form'] = MeasurementSearchForm
+        if len(self.request.GET) > 0:
+            context['search_form'] = MeasurementSearchForm(self.request.GET)
+        else:
+            context['search_form'] = MeasurementSearchForm()
+
         return context
 
     def get_queryset(self):
