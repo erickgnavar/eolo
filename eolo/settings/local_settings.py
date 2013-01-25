615b828878bb23716fa001f65d742f930d36d9d6
Erick Navarro <erickxls@gmail.com>
2013-01-25 00:18:29 -0500

    added local_settings

---
+++ eolo/settings/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eolo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
