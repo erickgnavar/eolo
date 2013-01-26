836700ec16d3b7bd9586c258eabdc4d42565b81e
Erick Navarro <erickxls@gmail.com>
2013-01-25 22:51:27 -0500

    fixed limit in pagination

--- apps/common/templatetags/common_tags.py
+++ apps/common/templatetags/common_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -32,3 +33,18 @@ def path_to_list(path):
 @register.filter
 def to_pipes(value):
     return '|' * len(value)
+
+
+@register.filter
+def pagination_range(page_obj):
+    num_pages = page_obj.paginator.num_pages
+    current = page_obj.number
+    variation = settings.PAGE_LENGTH / 2
+    min_value = current - variation
+    max_value = current + variation
+    print num_pages, max_value
+    if min_value <= 0:
+        return range(1, settings.PAGE_LENGTH)
+    if max_value >= num_pages:
+        return range(num_pages - settings.PAGE_LENGTH + 2, num_pages + 1)
+    return range(min_value + 1, max_value)
